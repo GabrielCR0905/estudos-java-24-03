8419a08b4822b3d84b002f80aad4ac4741effdfa
Gabriel de Carvalho Rodrigues <130592787+GabrielCR0905@users.noreply.github.com>
2025-03-24 16:25:51 -0300

    Add files via upload

---
+++ EstudosJava24_03/src/estudos/inical.java
@@ -0,0 +1,29 @@
+package estudos;
+
+public class inical {
+
+	public static void main(String[] args) {
+		int var = 2;
+		
+		switch(var) {
+		
+		case 1:
+			System.out.print("A varivel é menor que dois");
+			break;
+			
+		case 2:
+			System.out.print(" é variavel é menor que tres");
+			break;
+			
+		case 3:
+			System.out.print(" You Right!");
+			break;
+			
+		default:
+			System.out.print("Numero invalido!");
+			break;
+		}
+
+	}
+
+}
---
+++ EstudosJava24_03/src/estudos/whileee.java
@@ -0,0 +1,18 @@
+package estudos;
+
+public class whileee {
+
+	public static void main(String[] args) {
+		int var = 1;
+		
+		
+		while(var <= 4) {
+			System.out.print(var);
+			var--;
+			
+			
+		}
+
+	}
+
+}
